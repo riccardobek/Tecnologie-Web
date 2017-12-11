adc8c454477f816daae1543c2d0447f14eef9740
Michele Tagliabue <mikitg.michele@gmail.com>
2017-12-11 13:10:40 +0100

    Risolto bug del precedente commit

--- php/funzioni/funzioni_attivita.php
+++ php/funzioni/funzioni_attivita.php
@@ -1,6 +1,6 @@
 <?php
 function getMacroattivita($db) {
-    $query = $db->prepare("SELECT Codice, Nome, Descrizione, Immagine, Banner, REPLACE(LOWER(`Nome`), ' ', '-') AS Ancora FROM Macroattivita; FROM Macroattivita");
+    $query = $db->prepare("SELECT Codice, Nome, Descrizione, Immagine, Banner, REPLACE(LOWER(`Nome`), ' ', '-') AS Ancora FROM Macroattivita");
     $query->execute();
 
     $listaMacro = $query->fetchAll();
