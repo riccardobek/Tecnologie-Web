16c6a4cf37e8bace650b22aca16c718f7416be93
Michele Tagliabue <mikitg.michele@gmail.com>
2018-01-09 20:23:36 +0100

    aggiunto esperimento lettura QR

--- features.php
+++ features.php
@@ -1,7 +1,31 @@
 <?php
-/*
- *
- * TEST FUNZIONE DI HASH SU NUMERI
-*/
+require_once "php/database.php";
+require_once "php/funzioni/funzioni_attivita.php";
 
-echo "1-".substr(hash("sha256","1"),0,10);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Instascan</title>
+    <script type="text/javascript" src="js/instascan.min.js"></script>
+</head>
+<body>
+<video id="preview"></video>
+<script type="text/javascript">
+    var scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
+    scanner.addListener('scan', function (content) {
+        alert(content);
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+        } else {
+            console.error('No cameras found.');
+        }
+    }).catch(function (e) {
+        console.error(e);
+    });
+</script>
+</body>
+</html>
+
