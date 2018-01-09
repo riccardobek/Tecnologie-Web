d2c8d89e467cfb142a59afe1235e57a741b25e2a
Michele Tagliabue (dal fornello) <mikitg.michele@gmail.com>
2018-01-09 08:28:35 +0100

    test funzioni hash

--- features.php
+++ features.php
@@ -1,22 +1,7 @@
 <?php
 /*
  *
- * TEST DELLA FUNZIONE DI CONVALIDA DELLA DATA
+ * TEST FUNZIONE DI HASH SU NUMERI
 */
-$dataDaConvertire = "31/11/2017";
 
-if(!preg_match("/^(\d{2})\/(\d{2})\/(\d{4})$/",$dataDaConvertire))
-    return false;
-
-$matches = explode("/",$dataDaConvertire);
-$dataCalcolata = new DateTime(intval($matches[2])."-".intval($matches[1])."-".intval($matches[0]));
-
-//Se la data è nel formato corretto ma non è valida (ad esempio 31/02/2018) ritorno false
-if($dataCalcolata->format("d/m/Y") != $dataDaConvertire) {
-    echo "Data calcolata diversa: ";
-    print_r($dataCalcolata->format("d/m/Y"));
-    print_r($dataDaConvertire);
-    return false;
-}
-
-echo "TUTTAPOOOOST";
+echo "1-".substr(hash("sha256","1"),0,10);
--- php/delete_prenotazione.php
+++ php/delete_prenotazione.php
@@ -11,7 +11,7 @@ $idutente  = $_SESSION["utente"]["ID"];
 
 $deleteStatement = $db->prepare("DELETE FROM Prenotazioni WHERE IDAttivita = ? AND IDUtente = ? AND Giorno = ?");
 
-if($deleteStatement->execute($idattivita, $idutente, $data))
+if($deleteStatement->execute(array($idattivita, $idutente, $data)))
     $db->commit();
 else{
     $db->rollBack();
