bc7a7aba69d191940b197ef3b254c42118d242f8
DESKTOP-CT0CCV2\Riccardo <rikibek96@gmail.com>
2018-01-25 12:43:05 +0100

    Pannello admin completato: html e php. Da fare js e css(pochi accorgimenti)

--- pannello_admin.php
+++ pannello_admin.php
@@ -27,7 +27,9 @@ $HTML = str_replace("[#ATTIVITA]",stampaSchedeAttivita(), $HTML);
 $HTML = str_replace("[#ATTIVITA-PIU-PRENOTATE]",getAttivitaPiuPrenotate(),$HTML);
 
 //Rimpiazza segna posto [#PRENOTAZIONI]
-$HTML = str_replace("[#PRENOTAZIONI]",prenotazioniAttive(),$HTML);
+$HTML = str_replace("[#PRENOTAZIONI-ATTIVE]",prenotazioniAttive(),$HTML);
+$HTML = str_replace("[#PRENOTAZIONI-PASSATE]",prenotazioniPassate(),$HTML);
+
 
 //Footer
 $HTML = str_replace("[#MENU-MOBILE]",menuMobile($activeIndex),$HTML);
@@ -36,7 +38,7 @@ echo $HTML;
 //Funzione che crea la tabella degli utenti
 function listaUtenti(){
     global $db;
-    $listaUtenti = $db->prepare("SELECT Utenti.Nome as Nome, Utenti.Cognome as Cognome, Utenti.Indirizzo as Indirizzo, Utenti.Username as Username, Utenti.Email as Email FROM Utenti WHERE Utenti.Tipo='Utente'");
+    $listaUtenti = $db->prepare("SELECT Utenti.ID AS ID, Utenti.Nome as Nome, Utenti.Cognome as Cognome, Utenti.Indirizzo as Indirizzo, Utenti.Username as Username, Utenti.Email as Email FROM Utenti WHERE Utenti.Tipo='Utente'");
     $listaUtenti->execute();
     $risultato = $listaUtenti->fetchAll();
 
@@ -45,7 +47,7 @@ function listaUtenti(){
 
     foreach ($risultato as $r){
         $riga .= <<<SCRIVI
-<tr><td>{$r["Nome"]}</td><td>{$r["Cognome"]}</td><td>{$r["Indirizzo"]}</td><td>{$r["Username"]}</td><td>{$r["Email"]}</td><td><span class="btn-reimposta">Reimposta password</span></td></tr>
+<tr><td>{$r["Nome"]}</td><td>{$r["Cognome"]}</td><td>{$r["Indirizzo"]}</td><td>{$r["Username"]}</td><td>{$r["Email"]}</td><td><span class="btn-reimposta">Reimposta password</span></td><td><button id="{$r["ID"]}" class="btn-cancella">&#x1F5D1;</button></td></tr>
 SCRIVI;
         
     }
@@ -109,15 +111,31 @@ RIGA;
 function prenotazioniAttive(){
     global $db;
 
-    $prenotazioni=$db->prepare("SELECT Prenotazioni.Codice AS CodicePrenotazione, Utenti.Nome  AS Utente, Attivita.Nome AS Attivita, Prenotazioni.PostiPrenotati AS Posti, Prenotazioni.Giorno AS Giorno, Prenotazioni.Stato as Stato, Prenotazioni.Pagamento AS Pagato FROM Utenti, Attivita,Prenotazioni WHERE Utenti.ID=Prenotazioni.IDUtente AND Prenotazioni.IDAttivita=Attivita.Codice AND Giorno>=(SELECT CURDATE()) ORDER BY Giorno, Attivita, Utente ASC ");
+    $prenotazioni=$db->prepare("SELECT Prenotazioni.Codice AS CodicePrenotazione, Utenti.Nome AS Utente, Attivita.Nome AS Attivita, Prenotazioni.PostiPrenotati AS Posti, Prenotazioni.Giorno AS Giorno, Prenotazioni.Stato as Stato, Prenotazioni.Pagamento AS Pagato FROM Utenti, Attivita,Prenotazioni WHERE Utenti.ID=Prenotazioni.IDUtente AND Prenotazioni.IDAttivita=Attivita.Codice AND Giorno>=(SELECT CURDATE()) AND Prenotazioni.Stato<>'Cancellata' ORDER BY Giorno, Attivita, Utente ASC");
     $prenotazioni->execute();
     $arrayPrenotazioni=$prenotazioni->fetchAll();
     $row="";
     foreach ($arrayPrenotazioni as $riga){
         $row .= <<<RIGA
-        <tr><td>{$riga["Utente"]}</td><td>{$riga["Attivita"]}</td><td>{$riga["Posti"]}</td><td>{$riga["Giorno"]}</td><td>{$riga["Stato"]}</td><td>{$riga["Pagato"]}</td><td><button id="{$riga["CodicePrenotazione"]}" class="btn-cancella">CANCELLA PRENOTAZIONE</button></td></tr>
+        <tr><td>{$riga["Utente"]}</td><td>{$riga["Attivita"]}</td><td>{$riga["Posti"]}</td><td>{$riga["Giorno"]}</td><td>{$riga["Stato"]}</td><td>{$riga["Pagato"]}</td><td>&#x270E;</td><td><button id="{$riga["CodicePrenotazione"]}" class="btn-cancella">&#x1F5D1;</button></td></tr>
 RIGA;
     }
     return $row;
 }
+
+function prenotazioniPassate(){
+    global $db;
+
+    $prenotazioni=$db->prepare("SELECT Prenotazioni.Codice AS CodicePrenotazione, Utenti.Nome  AS Utente, Attivita.Nome AS Attivita, Prenotazioni.PostiPrenotati AS Posti, Prenotazioni.Giorno AS Giorno, Prenotazioni.Stato as Stato, Prenotazioni.Pagamento AS Pagato FROM Utenti, Attivita,Prenotazioni WHERE Utenti.ID=Prenotazioni.IDUtente AND Prenotazioni.IDAttivita=Attivita.Codice AND Giorno<(SELECT CURDATE()) ORDER BY Giorno, Attivita, Utente ASC ");
+    $prenotazioni->execute();
+    $arrayPrenotazioni=$prenotazioni->fetchAll();
+    $row="";
+    foreach ($arrayPrenotazioni as $riga){
+        $row .= <<<RIGA
+        <tr><td>{$riga["Utente"]}</td><td>{$riga["Attivita"]}</td><td>{$riga["Posti"]}</td><td>{$riga["Giorno"]}</td><td>{$riga["Stato"]}</td><td>{$riga["Pagato"]}</td></tr>
+RIGA;
+    }
+    return $row;
+}
+
 ?>
