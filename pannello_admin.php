5ecd89ee576204598f052c983214381ca3081366
DESKTOP-CT0CCV2\Riccardo <rikibek96@gmail.com>
2018-02-06 10:07:02 +0100

    Piccola miglioria storico prenotazioni

--- pannello_admin.php
+++ pannello_admin.php
@@ -155,6 +155,7 @@ function prenotazioniPassate(){
 FROM Utenti, Attivita,Prenotazioni WHERE Utenti.ID=Prenotazioni.IDUtente AND Prenotazioni.IDAttivita=Attivita.Codice AND Giorno<(SELECT CURDATE()) ORDER BY Giorno, Attivita, Utente ASC ");
     $prenotazioni->execute();
     $arrayPrenotazioni=$prenotazioni->fetchAll();
+    impostaTestoPagamento($prenotazioni);
     $row="";
     foreach ($arrayPrenotazioni as $riga){
         $row .= <<<RIGA
