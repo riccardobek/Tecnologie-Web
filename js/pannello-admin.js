d26bc3f2141ee82f47dd0d5515aca3ce19d77892
Bolzo <bolzo96@gmail.com>
2018-02-11 20:55:16 +0100

    commenti

--- js/pannello-admin.js
+++ js/pannello-admin.js
@@ -179,23 +179,8 @@ $(function() {
             }
         });
     });
-    /*
-    var etichette = [];
-    var valoriGrafico = [];
 
-    $(".numero-prenotazioni").each(function(){
-        etichette.push($(this).data("target"));
-        valoriGrafico.push(parseInt($(this).text()));
-    });
-
-    //Statistiche
-    var data = {
-        labels: etichette,
-        series: valoriGrafico
-    };
-
-    new Chartist.Pie('.ct-chart', data);
-    */
+    //Tasto conferma pagamento
     $(".pay").click(function (e){
         e.preventDefault();
         e.stopPropagation();
